52ea68b4cd5f141def32a09e2c263531c000acae
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2025-12-11 13:28:40 -0800

    Update test execution with additional test data
    Update smoke tests and expected output to support additional tests cases

--- test/smoke-tests.sh
+++ test/smoke-tests.sh
@@ -18,12 +18,12 @@ if [ -z "$LEGACY_REGISTRY_HOME" ] || [ -z "$LEGACY_HARVEST_HOME" ] || [ -z "$LEG
   exit 1
 fi
 
-OUTDIR=$DATA_HOME/test1
+OUTDIR=$DATA_HOME/test/pds4
 mkdir -p $OUTDIR
 
 # Install Solr with preconfigured collections
-#$LEGACY_REGISTRY_HOME/bin/registry_legacy_installer_docker.sh install
-#check_status $? "[ERROR] Registry Manager Install Failure"
+$LEGACY_REGISTRY_HOME/bin/registry_legacy_installer_docker.sh install
+check_status $? "[ERROR] Registry Manager Install Failure"
 
 echo "+++ TEST 1 - PDS4 +++"
 TEST_DATA_HOME=$PARENTDIR/src/test/resources/data/pds4
@@ -38,16 +38,18 @@ check_status $? "[ERROR] Harvest Failure"
 echo "[SUCCESS] Harvest Successful"
 
 echo "[INFO] Check solr docs match expected"
-SOLR_EXPECTED=$PARENTDIR/src/test/resources/data/pds4/expected/solr_doc_expected.xml
+SOLR_EXPECTED=$PARENTDIR/src/test/resources/data/expected/pds4/solr_doc_expected.xml
 # Cleanse output file of elements that are specific to where the code is run
 egrep -v "package_id|file_ref_location|file_ref_url|modification_date" $OUTDIR/solr-docs/solr_doc_0.xml > solr_doc_actual.xml
 
+
 # Diff
 test=$(diff solr_doc_actual.xml $SOLR_EXPECTED)
-if [ -z "$test" ]; then
+if [ -z "$test" ] && [ -f $OUTDIR/solr-docs/solr_doc_0.xml ]; then
     echo "[SUCCESS] Harvest Solr Doc Diff Successful"
 else
     echo "[ERROR] Harvest Solr Doc Diff Test Failed"
+    echo "$(pwd)/solr_doc_actual.xml vs. $SOLR_EXPECTED"
     echo $test
     exit 1
 fi
@@ -59,12 +61,10 @@ $LEGACY_REGISTRY_HOME/bin/registry-mgr-solr $OUTDIR/solr-docs/
 
 check_status $? "[ERROR] Registry Manager Load Failure"
 
-exit
-
 echo "+++ Test 2 - PDS3 +++"
-TEST_DATA_HOME=$PARENTDIR/src/test/resources/data/test2/junosru/
+TEST_DATA_HOME=$PARENTDIR/src/test/resources/data/pds3/
 #TEST_DATA_HOME=${TEST_DATA_HOME//\//\\\/}
-OUTDIR=$DATA_HOME/test2
+OUTDIR=$DATA_HOME/test/pds3
 mkdir -p $OUTDIR
 
 echo "[INFO] Parse test data with Catalog Tool"
@@ -77,13 +77,13 @@ check_status $? "[ERROR] Catalog Solr Failure"
 echo "[SUCCESS] Catalog Solr Successful"
 
 echo "[INFO] Check solr docs match expected"
-SOLR_EXPECTED=$PARENTDIR/src/test/resources/data/test2/expected/solr_doc_expected.xml
+SOLR_EXPECTED=$PARENTDIR/src/test/resources/data/expected/pds3/solr_doc_expected.xml
 # Cleanse output file of elements that are specific to where the code is run
-egrep -v "package_id|file_ref_location|file_ref_url|modification_date" $OUTDIR/usa_nasa_pds_jnosru_0xxx__jnosruedr.solr.xml > solr_doc_actual.xml
+egrep -v "package_id|file_ref_location|file_ref_url|modification_date" $OUTDIR/usa_nasa_pds_jnowav_0000__jnowavs1000.solr.xml > solr_doc_actual.xml
 
 # Diff
 test=$(diff solr_doc_actual.xml $SOLR_EXPECTED)
-if [ -z "$test" ]; then
+if [ -z "$test" ] && [ -f $OUTDIR/usa_nasa_pds_jnowav_0000__jnowavs1000.solr.xml ]; then
     echo "[SUCCESS] Catalog Solr Doc Diff Successful"
 else
     echo "[ERROR] Catalog Solr Doc Diff Test Failed"
